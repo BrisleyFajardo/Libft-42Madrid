2ff9c168a1cd37015c54b761245dd1de405a06d2
Bris <test.brisley@gmail.com>
2024-03-17 23:45:49 +0100

    Archivo header

---
+++ libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfajard <brfajard@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 12:26:07 by brfajard          #+#    #+#             */
+/*   Updated: 2024/03/17 14:29:43 by brfajard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_isalpha(int c);
+
+int	ft_isdigit(int c);
+
+int	ft_isalnum(int c);
+
+int	ft_isascii(int c);
+
+int	ft_isprint(int c);
+
+#endif
