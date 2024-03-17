56a63d409284f5ea0fd95ecdb64802f39f5ad741
Bris <test.brisley@gmail.com>
2024-03-17 23:47:09 +0100

    Main de prueba

---
+++ main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfajard <brfajard@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 18:49:42 by brfajard          #+#    #+#             */
+/*   Updated: 2024/03/17 18:49:46 by brfajard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+/*
+	ftisalpha
+    printf("El resultado: %d\n", ft_isalpha(38));
+
+    ftisdigit
+    
+    int result;
+    result = ft_isdigit(50);
+    printf("El resultado: %d\n", result);
+    
+
+    ft_isalnum
+    printf("El resultado: %d\n", ft_isalnum(' '));
+
+    ft_isascii
+    printf("El resultado: %d\n", ft_isascii(128));
+
+    ft_isprint
+    printf("El resultado: %d\n", ft_isprint(198));
+*/
+	return (0);
+}
